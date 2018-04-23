0dd63364fc2dab01107722b569346d4384584b74
Sathish Yerramsetti <sayerramsetti@paypal.com>
2018-04-24 01:31:24 +0530

    passing props and children and validating props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 export class Home extends React.Component {
 	render() {
@@ -12,7 +13,25 @@ export class Home extends React.Component {
 				{ 2 + 2 }
 				{ content }
 				{ 5 == 2 ? "YES" : "NO"}
+				<p> Your name is {this.props.name}, 
+					Your age is {this.props.age}</p>
+				<p>User Object => Name: {this.props.user.name}</p>
+				<div>
+					<h4>Hobbies</h4>
+					<ul>
+						{this.props.user.hobbies.map((hobby, index) => <li key={index}>{hobby}</li>)}
+					</ul>
+				</div>
+				<hr/>
+				{this.props.children}
 			</div>
 		);
 	}
 }
+
+Home.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number,
+	user: PropTypes.object,
+	children: PropTypes.element.isRequired
+};
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,10 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
 	render() {
+		let user = {
+			name: "YS",
+			hobbies: ["Coding", "Sports"]
+		};
 		return(
 			<div className="container">
 				<div className="row">
@@ -14,7 +18,9 @@ class App extends React.Component {
 				</div>
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Home/>
+						<Home name={"Sathish"} age={30} user={user}>
+							<p>This is paragraph </p>
+						</Home>
 					</div>
 				</div>
 			</div>
