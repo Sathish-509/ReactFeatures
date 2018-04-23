e4cec9c1a560b9b60adbff15d9d0d7d32267541f
Sathish Yerramsetti <sayerramsetti@paypal.com>
2018-04-23 23:53:28 +0530

    dynamic content in jsx

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,16 @@ import React from "react";
 
 export class Home extends React.Component {
 	render() {
+		let content = "";
+		if (true) {
+			content = <p> Hello! </p>
+		}
 		return (
 			<div>
 				<p>In a Home component</p>
+				{ 2 + 2 }
+				{ content }
+				{ 5 == 2 ? "YES" : "NO"}
 			</div>
 		);
 	}
