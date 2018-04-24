5482665a8cf200dd1e1fa5be5c8208d7a499d611
Sathish Yerramsetti <sayerramsetti@paypal.com>
2018-04-24 23:39:05 +0530

    Passing Data between Parent and Child Component

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -6,7 +6,8 @@ export class Home extends React.Component {
 		super();
 		this.state = {
 			age: props.age,
-			status: 0
+			status: 0,
+			homeLink: "Changed Link"
 		};
 		setTimeout(() => {
 			this.setState({
@@ -14,11 +15,17 @@ export class Home extends React.Component {
 			});
 		}, 3000);
 	}
+
 	onMakeOlder() {
 		this.setState({
 			age: this.state.age + 3
 		});
 	}
+
+	onChangeLink() {
+		this.props.changeLink(this.state.homeLink);
+	}
+
 	render() {
 		return (
 			<div>
@@ -33,6 +40,10 @@ export class Home extends React.Component {
 				<hr/>
 				<button onClick={this.props.greet} className="btn btn-primary">
 				Greet
+				</button>
+				<hr/>
+				<button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">
+				Change Home Link Name
 				</button>
 			</div>
 		);
--- src/app/index.js
+++ src/app/index.js
@@ -4,22 +4,39 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			homeLink: "Home"
+		}
+	}
+
 	onGreet(){
 		alert('Hello!!');
 	}
+
+	onChangeLinkName(newName) {
+		this.setState({
+			homeLink: newName
+		});
+	}
+
 	render() {
 		return(
 			<div className="container">
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Header homeLink="Home"/>
+						<Header homeLink={this.state.homeLink}/>
 					</div>
 				</div>
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Home name={"Sathish"} age={30} greet={this.onGreet}>
-							<p>This is paragraph </p>
-						</Home>
+						<Home 
+							name={"Sathish"} 
+							age={30} 
+							greet={this.onGreet}
+							changeLink={this.onChangeLinkName.bind(this)}
+						/>					
 					</div>
 				</div>
 			</div>
