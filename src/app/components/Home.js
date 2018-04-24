bd761bec42bba89475d861148c3b3a57fbecfc87
Sathish Yerramsetti <sayerramsetti@paypal.com>
2018-04-24 22:58:09 +0530

    react dom updates

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,18 +4,28 @@ import PropTypes from 'prop-types'
 export class Home extends React.Component {
 	constructor(props){
 		super();
-		this.age = props.age;
+		this.state = {
+			age: props.age,
+			status: 0
+		};
+		setTimeout(() => {
+			this.setState({
+				status: 1
+			});
+		}, 3000);
 	}
 	onMakeOlder() {
-		this.age += 3;
-		console.log("age :"+this.age);
+		this.setState({
+			age: this.state.age + 3
+		});
 	}
 	render() {
 		return (
 			<div>
 				<p>In a Home component</p>
 				<p> Your name is {this.props.name}, 
-					Your age is {this.props.age}</p>
+					Your age is {this.state.age}</p>
+				<p>Status : {this.state.status}</p>
 				<hr/>
 				<button onClick={() => this.onMakeOlder()} className="btn btn-primary">
 				Make an Older!
